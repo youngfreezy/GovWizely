3f0002fc9e16c1d797dbc9674d3a8df437235b30
Fareez Ahmed <fareezahmed@Fareezs-MacBook-Pro.local>
2018-12-11 19:39:41 -0500

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import HomeView from "./containers/HomeView";
+import DetailView from "./containers/DetailView";
+import {
+    expect
+} from 'chai';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render( < App / > , div);
+    ReactDOM.unmountComponentAtNode(div);
 });
+
+it('the home page renders correctly', () => {
+    const div = document.createElement('div');
+    ReactDOM.render( < HomeView / > , div);
+    ReactDOM.unmountComponentAtNode(div);
+})
+
+it('the detail page renders correctly', () => {
+    const div = document.createElement('div');
+    ReactDOM.render( < DetailView / > , div);
+    ReactDOM.unmountComponentAtNode(div);
+})
+
+it('the detail page checks the slug info on component mount', () => {
+    let detail = new DetailView()
+    detail.props = {
+        location: {
+            pathname: "blah"
+        }
+    }
+    detail.componentDidMount();
+})
+
+it('the home page sets the default filters to articles', () => {
+    let home = new HomeView();
+    home.componentDidMount();
+    expect(home.state.contentType).to.equal('articles');
+
+})
+
+it('the columns in the table are rendered properly', () => {
+    let home = new HomeView();
+    expect(home.columns.length).to.equal(3);
+})
+
+it('the posts are populated on component mount', () => {
+    let home = new HomeView();
+    expect(home.getAbridgedPosts()).to.not.be.undefined;
+    expect(home.getContentType()).to.not.be.undefined;
+})
--- src/containers/DetailView.js
+++ src/containers/DetailView.js
@@ -1,32 +1,40 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import axios from 'axios';
 
 class DetailView extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      detailView: {}
-    }
-  }
-  componentDidMount() {
-    //replace trailing slash with .json
-    let slug = this.props.location.pathname.slice(0, -1) + ".json";
-    axios.get(`https://www.healthcare.gov/${slug}`)
-    .then(response => {
-      this.setState({detailView: response.data})
-    })
-    .catch(err => {
-      throw new Error(err);
-    });
-  }
- 
 
-  render(props) {
-    return (
-      <pre className="detail-view"> {JSON.stringify(this.state.detailView, null, 4)} </pre>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            detailView: {}
+        }
+    }
+    componentDidMount() {
+        //replace trailing slash with .json
+        if (this.props.location) {
+            let slug = this.props.location.pathname.slice(0, -1) + ".json";
+            return axios.get(`https://www.healthcare.gov/${slug}`)
+                .then(response => {
+                    this.setState({
+                        detailView: response.data
+                    })
+                })
+                .catch(err => {
+                    throw new Error(err);
+                });
+        }
+    }
+
+
+    render(props) {
+        return ( 
+          <pre className = "detail-view" > 
+            { JSON.stringify(this.state.detailView, null, 4)} 
+          < /pre>
+        )
+    }
 }
 
 export default DetailView;
